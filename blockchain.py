bb46d3421247b29e76204a40d53f8e3670be4019
Alan Hurtarte <alan@lionmane.io>
2018-09-09 13:40:26 -0600

    Added get previous block function

--- blockchain.py
+++ blockchain.py
@@ -21,5 +21,8 @@ class Blockchain:
         self.chain.append(block)
         
         return block
+    
+    def get_previous_block(self):
+        return self.chain[-1]
 
 # Part 2 - Mining our blockchain
