fdaafa670c02cde7bb3065fc6ac57ec0b2aa8b49
Alan Hurtarte <alan@lionmane.io>
2018-09-09 13:36:57 -0600

    Define function to create block

---
+++ blockchain.py
@@ -0,0 +1,25 @@
+#module 1
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+# Part 1 Building a blockchain
+
+class Blockchain:
+    
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = '0')
+        
+    def create_block(self, proof, previous_hash):
+        block = {'index': len(self.chain) + 1,
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'previous_hash': previous_hash}
+        
+        self.chain.append(block)
+        
+        return block
+
+# Part 2 - Mining our blockchain
