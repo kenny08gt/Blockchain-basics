099981cceedf595ff23b16dda7fb6708cb0ba1fc
Alan Hurtarte <alan@lionmane.io>
2018-09-09 18:03:24 -0600

    Finish blockchain class

--- blockchain.py
+++ blockchain.py
@@ -25,7 +25,7 @@ class Blockchain:
     def get_previous_block(self):
         return self.chain[-1]
 
-    def proof_of_word(self, previous_proof):
+    def proof_of_work(self, previous_proof):
         new_proof = 1
         check_proof = False
         while check_proof is False:
@@ -37,4 +37,58 @@ class Blockchain:
                 
         return new_proof
         
+    def hash(self, block):
+        encoded_block = json.dumps(block, sort_keys = True).encode()
+        return hashlib.sha256(encoded_block).hexdigest()
+    
+    def is_chain_valid(self, chain):
+        previous_block = chain[0]
+        block_index = 1
+        while block_index < len(chain):
+            block = chain[block_index]
+            if block['previous_hash'] != self.hash(previous_block):
+                return False
+            
+            previous_proof = previous_block['proof']
+            proof = block['proof']
+            
+            hash_operation = hashlib.sha256(str(proof**2 - previous_proof**2).encode()).hexdigest()
+            
+            if hash_operation[:4]  != '0000':
+                return False
+            
+            previous_block = block
+            block_index += 1
+            
+        return True
+        
 # Part 2 - Mining our blockchain
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
